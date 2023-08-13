9bd4c87402f5e81aa49f52172aec86cd140ded75
Caio Costa Cavalcante <nextc3@gmail.com>
2023-08-12 23:21:32 -0300

    Classe Usabilidade criada

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,25 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+        let resposta;
+
+        return resposta;
     }
 
 }
 
 export { CaixaDaLanchonete };
+
+
+class Usabilidade {
+  static separarString(stringComVirgula) {
+    const [parte1, parte2] = stringComVirgula.split(',');
+    return [parte1, Number(parte2)];
+  }
+
+  static juntarString(string, numero) {
+    return `${string} ${numero}`;
+}
+}
+export {Usabilidade}
+
